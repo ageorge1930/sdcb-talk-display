d4c0bf8e6b3f9ad42754d12f9c9bd5cd748d8c15
ageorge1930 <ageorge@sdcb.org>
2026-07-16 15:31:33 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,33 @@
-const CACHE_NAME = 'talk-display-v1';
-const FILES_TO_CACHE = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE_NAME = 'talk-display-v2';
+const FILES_TO_CACHE = [
+  './index.html', './manifest.json', './icon-192.png', './icon-512.png',
+  './asl-A.svg',
+  './asl-B.svg',
+  './asl-C.svg',
+  './asl-D.svg',
+  './asl-E.svg',
+  './asl-F.svg',
+  './asl-G.svg',
+  './asl-H.svg',
+  './asl-I.svg',
+  './asl-J.svg',
+  './asl-K.svg',
+  './asl-L.svg',
+  './asl-M.svg',
+  './asl-N.svg',
+  './asl-O.svg',
+  './asl-P.svg',
+  './asl-Q.svg',
+  './asl-R.svg',
+  './asl-S.svg',
+  './asl-T.svg',
+  './asl-U.svg',
+  './asl-V.svg',
+  './asl-W.svg',
+  './asl-X.svg',
+  './asl-Y.svg',
+  './asl-Z.svg'
+];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
@@ -9,7 +37,11 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', (event) => {
