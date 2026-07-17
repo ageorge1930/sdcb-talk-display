63241a98c2e57601375ba44e14a2426147ce44da
ageorge1930 <ageorge@sdcb.org>
2026-07-16 17:10:24 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'talk-display-v2';
+const CACHE_NAME = 'talk-display-v3';
 const FILES_TO_CACHE = [
   './index.html', './manifest.json', './icon-192.png', './icon-512.png',
   './asl-A.svg',
